26b5bfa089b80eb71e02c315ed78fc68205a1e03
Ryan <rkim20@choate.edu>
2018-09-10 14:21:46 -0400

    Details

---
+++ homework1.py
@@ -0,0 +1,25 @@
+print ("Hello there, I am a computer!")
+name = input("What is your name? ")
+print ("Greetings, " + name + "!")
+feeling = input("How are you feeling today, " + name + "? ")
+if (feeling == "good"):
+	print ("Awesome! Same!")
+elif (feeling == "Good"):
+	print ("Awesome! Same!")
+elif (feeling == "great"):
+	print ("Awesome! Same!")
+elif (feeling == "Great"):
+	print ("Awesome! Same!")
+elif (feeling == "bad"):
+	print ("I'm sorry to hear that.")
+elif (feeling == "Bad"):
+	print ("I'm sorry to hear that.")
+else:
+	print ("I'm happy you're feeling very " + feeling + " today!")
+
+
+#https://docs.python.org/3/tutorial/controlflow.html
+#else if statements
+
+#http://anh.cs.luc.edu/handsonPythonTutorial/ifstatements.html
+#if statements
