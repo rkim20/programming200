53d5acf433465d5b62bb87d3a5670451d2490d18
Ryan <rkim20@choate.edu>
2018-10-10 20:29:34 -0400

    Minesweeper

--- minesweeperproject.py
+++ minesweeperproject.py
@@ -1,7 +1,8 @@
 #Ryan Kim
-#Monday, October 1st
-#Minesweeper Field - I had trouble with the ranges of things as well as the look around function. I searched it up but didn't fully grasp how to use it.
-#https://stackoverflow.com/questions/2973436/regex-lookahead-lookbehind-and-atomic-groups  -  Look Around Function
+#Wednesday, October 10th
+#This project was extremely difficult for me. There were parts that were simple and easy, but there were also parts that took me a lot of time and effort. Specifically, the zero checking was a part that I struggled a lot with. Using lists and nested for loops to check around the zero and updating variables was all really hard to put together. I originally started with a lot of if statements, but after a while, I realized it would take a significant number of these statements for the program to do what I wanted. Implementing the for loops wasn't too complicated, but there was always something that didn't quite click with the code and finding and fixing the problem posed the biggest challenge. Overall, I think the project was helpful in learning and experiencing different ways to code, but I think splitting the assignment into more homework assignments would help students have a better steady track.
+#On my honor, I have neither given nor received unauthorized aid.
+
 import sys
 import random
 
@@ -15,6 +16,7 @@ def reveal():
 	choiceX = int(choiceX)
 	choiceY = int(choiceY)
 	#-----------------------------------------------
+	#If revealed point is a bomb
 	if (field[choiceY][choiceX] == "B"):
 		for y in range (1, height-1):
 			for x in range(1, width-1):
@@ -22,7 +24,9 @@ def reveal():
 			print("")
 
 		print("You hit a bomb! You lost!")
+
 	#-----------------------------------------------
+	#If revealed point is a 0, reveal all other zeroes around it
 	elif (field[choiceY][choiceX] == "0"):
 
 		points = []
@@ -56,38 +60,30 @@ def reveal():
 				print(board[y][x], end = " ")
 			print("")
 
-
-
-
-		#The choiceY and choiceX variables are not updating so the loop repetitively adds the same points to the list
-			#choiceY and choiceX stay the same throughout the loop
-
-			"""
-			Solutions
-			- Instead of value [choiceY + y] have position (1)
-			- if (k==1) for first time and then elif (k==0) for second and adjust everything for position
-			"""
-
 	#-----------------------------------------------
-	elif (field[choiceY][choiceX] != "B") and (field[choiceY][choiceX] != "0"): #also could put else instead of elif statement
+	#If revealed point is a number
+	else:
 		board[choiceY][choiceX] = field[choiceY][choiceX]
 
 		for y in range (1, height-1):
 			for x in range(1, width-1):
 				print(board[y][x], end = " ")
 			print("")
-	#print board without any other point showing
 #----------------------------------------------------------------
 def flag():
 	global flagX, flagY, flagC, flagW, height, width
 
-	flagX = input("Give the x position you would like to flag: ")
-	flagY = input("Give the y position you would like to flag: ")
+	flagX = input("\nGive the x position you would like to flag: ")
+	flagY = input("\nGive the y position you would like to flag: ")
 
 	flagX = int(flagX)
 	flagY = int(flagY)
 
-	board[flagY][flagX] = "F"
+	if (board[flagY][flagX] == "#"):
+		board[flagY][flagX] = "F"
+	else:
+		print("\nYou flagged a revealed point. Try again!\n")
+		flag()
 
 	if (field[flagY][flagX] == "B"):
 		flagC += 1
@@ -104,8 +100,8 @@ def flag():
 def removeFlag():
 	global removeX, removeY, flagW, flagC, height, width
 
-	removeX = input("Give the x position you would like to remove the flag from: ")
-	removeY = input("Give the y position you would like to remove the flag from: ")
+	removeX = input("\nGive the x position you would like to remove the flag from: ")
+	removeY = input("\nGive the y position you would like to remove the flag from: ")
 
 	removeX = int(removeX)
 	removeY = int(removeY)
@@ -125,16 +121,12 @@ def removeFlag():
 		print("You chose a point that wasn't flagged! Try again!")
 		removeFlag()
 
-	#if the point had a "#" before it was removed, put a "#" back in
-	#if the point had a number before it was removed, put a number back in
 	board[removeY][removeX] = "#"
 
 	for y in range (1, height-1):
 			for x in range(1, width-1):
 				print(board[y][x], end = " ")
 			print("")
-
-#What about past points? Previously selected flagged values?
 #----------------------------------------------------------------
 def win():
 	print("\n\nYou win!")
@@ -142,7 +134,11 @@ def win():
 #----------------------------------------------------------------
 
 
-#Game Setup
+#Game Setup and Intro
+
+print("\nWelcome to Minesweeper! Flag all the bombs to win the game!\n")
+
+#Setup for board
 width = input("Width: ")
 height = input("Height: ")
 bombs = input("Bombs: ")
@@ -156,12 +152,10 @@ height += 2
 
 #What player sees
 board = []
-
 board = [["#"]*width for x in range(height)]
 
 #Solution
 field = []
-
 field = [["0"]*width for x in range(height)]
 
 
@@ -196,7 +190,7 @@ while (count < bombs):
 
 	count += 1
 
-#Places Bombs and Adds Numbers
+#Places Bombs and Adds Numbers Around
 for y in range(1, height-1):
 	for x in range(1, width-1):
 		if (field[y][x] == "B"):
@@ -259,7 +253,7 @@ for y in range(1, height-1):
 
 #--------------------------------------------------------------------------------------
 
-#Game Start
+#Options
 playing = True
 
 while (playing):
@@ -273,33 +267,24 @@ while (playing):
 	choice = input("\nChoose your option:\n 1.) Reveal - Choose a spot on the board and reveal whether there is a bomb or not\n 2.) Flag - Mark a spot on the board where you think there is a bomb\n 3.) Remove Flag - Choose a flagged spot that you would like to remove the flag from\n\n Option: ")
 
 
-	if (choice == "1") or (choice == "reveal")  or (choice == "Reveal"):
+	if (choice == "1") or (choice == "reveal") or (choice == "Reveal") or (chocie == "R") or (choice == "r"):
 		reveal()
 
-	if (choice == "2") or (choice == "flag") or (choice == "Flag"):
+	if (choice == "2") or (choice == "flag") or (choice == "Flag") or (choice == "F") or (choice == "f"):
 		flag()
 
 	if (choice == "3") or (choice == "removeflag") or (choice == "remove flag") or (choice == "Remove flag") or (choice == "Remove Flag"):
 		removeFlag()
-#-----------------------------------------------
 
 
 
 
-"""
-if (field[y-1][x+1] != "B"):
-				field[y-1][x+1] = int(field[y-1][x+1])
-				field[y-1][x+1] += 1
-"""
-
-"""
-for y in range (1, height-1):
-	for x in range(1, width-1):
-		print(field[y][x], end = " ")
-	print("")
-"""
 
 
 
-#1.) Corner numbers - adding 2 to h and w
-#2.) Adding more than 1 bomb - removing and inserting 0 - need to regenerate random numbers
+
+
+
+
+
+
