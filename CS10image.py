d5ab8c1c11d2dc2df2fdf56983c83074c839787a
Ryan <rkim20@choate.edu>
2018-10-14 21:57:30 -0400

    images

--- CS10image.py
+++ CS10image.py
@@ -5,6 +5,14 @@ imgy = 512
 
 image = Image.new("RGB",(imgx,imgy))
 
-image.putpixel((0,0),(255,0,0))
+c = int(255)
+
+for x in range(512):
+	for y in range(512):
+		image.putpixel((x,y),(c,0,0))
+
+		c -= 1
+		if (c == 0):
+			c = 255
 
 image.save("demo_image.png", "PNG")
