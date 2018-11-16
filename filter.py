2c245cae24833d09b6bf546e1e22c59cf9888762
Ryan <rkim20@choate.edu>
2018-11-16 11:58:45 -0500

    Filter

--- filter.py
+++ filter.py
@@ -1,6 +1,7 @@
 #Initiation
 from PIL import Image
 from PIL import ImageFilter
+import colorsys
 
 
 #Functions
@@ -8,12 +9,12 @@ from PIL import ImageFilter
 
 #Main--------------------------------------------------------------------
 
-image = Image.open('filterphoto.jpg').convert('L')
+image = Image.open('photo2.jpg').convert('L')
 width, height = image.size
 
 image = image.filter(ImageFilter.SMOOTH_MORE)
 
-image.show()
+#image.show()
 
 #-------------------------------------------------------------------------
 
@@ -33,6 +34,54 @@ for x in range (width):
 		image.putpixel((x,y),(r*2, g, b))
 
 
-image = image.filter(ImageFilter.EMBOSS)
+#image = image.filter(ImageFilter.EMBOSS)
 
 #image.show()
+
+#-------------------------------------------------------------------------
+"""
+image  = Image.open('photo3.jpg')
+width, height = image.size
+
+for x in range (width):
+	for y in range (height):
+		r, g, b = image.getpixel((x,y))
+
+		r = int(r/255)
+		g = int(g/255)
+		b = int(b/255)
+
+		h, s, v = colorsys.rgb_to_hsv(r,g,b)
+
+		h = int(h*255)
+		s = int(s*100)
+		v = int(v*100)
+
+		if (s <= 60):
+			s += 20
+
+		h = h/255
+		s = s/100
+		v = v/100
+
+		r, g, b = colorsys.hsv_to_rgb(h,s,v)
+
+		r = int(r*255)
+		g = int(g*255)
+		b = int(b*255)
+
+		image.putpixel((x,y), (r,g,b))
+
+
+image.show()
+"""
+
+
+
+
+
+
+
+
+
+
